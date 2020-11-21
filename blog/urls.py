884f64b31d74a674d002a51554dcdd6b6d6a87b4
Ariel <juanarielcruzgarcia@gmail.com>
2020-11-20 22:35:19 -0400

    Mi aplicación sistema, primer commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('index/', inicio, name='index'),
-    path('base/', base, name = "base"),
+    path('', base, name = "base"),
     path('Creador/', CreatePlaca.as_view(), name="Creador"),
     path('Listado/', ListPlaca, name="Listado"),
     path('Modificar/<int:pk>', UpdatePlaca.as_view(), name="Modificar"),
